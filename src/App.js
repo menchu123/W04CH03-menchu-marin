46a9a2c37f27dc9065344483daa356f8bc867015
Menchu MV <mcmvillalobos@gmail.com>
2021-10-21 00:27:40 +0200

    Organized components

--- src/App.js
+++ src/App.js
@@ -1,84 +1,33 @@
 import "./App.css";
 import Info from "./components/Info/Info.js";
 import Display from "./components/Display/Display";
-import Action from "./components/Action/Action";
 import Keyboard from "./components/Keyboard/Keyboard";
 import Actions from "./components/Actions/Actions";
 
 function App() {
   return (
     <>
-      <Info isCalling={false} />
       {/* <Key text="ok" actionOnClick={() => console.log("ji")} isCalling={true} /> */}
-      <Display numbers={[6, 4, 2, 7, 9]} />
-      <div className="actions">
-        <Action
-          isDisabled={false}
-          text={"llamar"}
-          actionOnClick={() => console.log("hola")}
-          className={"call"}
-        />
+      <div className="container">
+        {/* <!-- El siguiente elemento se oculta añadiéndole la clase "off" --> */}
+        <Info isCalling={false} />
+        <main className="phone">
+          <div className="keyboard-container">
+            <ol className="keyboard">
+              <Keyboard isCalling={false} />
+            </ol>
+          </div>
+          <div className="actions">
+            <Display numbers={[6, 4, 2, 7, 9]} />
+            {/* <!-- El botón de llamar debe tener la clase "activo" cuando --> */}
+            {/* <!-- el número de teléfono tiene 9 cifras --> */}
+
+            {/* <!-- Sólo se tiene que ver un botón u otro --> */}
+            <Actions isDisabled={false} isCalling={false} />
+          </div>
+        </main>
       </div>
-      <ol className="keyboard">
-        <Keyboard isCalling={false} />
-      </ol>
-      <Actions isDisabled={false} isCalling={false} />
     </>
-    // <div className="container">
-    //   {/* <!-- El siguiente elemento se oculta añadiéndole la clase "off" --> */}
-    //   <span className="message">Calling...</span>
-    //   <main className="phone">
-    //     <div className="keyboard-container">
-    //       <ol className="keyboard">
-    //         <li>
-    //           <button className="key">1</button>
-    //         </li>
-    //         <li>
-    //           <button className="key">2</button>
-    //         </li>
-    //         <li>
-    //           <button className="key">3</button>
-    //         </li>
-    //         <li>
-    //           <button className="key">4</button>
-    //         </li>
-    //         <li>
-    //           <button className="key">5</button>
-    //         </li>
-    //         <li>
-    //           <button className="key">6</button>
-    //         </li>
-    //         <li>
-    //           <button className="key">7</button>
-    //         </li>
-    //         <li>
-    //           <button className="key">8</button>
-    //         </li>
-    //         <li>
-    //           <button className="key">9</button>
-    //         </li>
-    //         <li>
-    //           <button className="key">0</button>
-    //         </li>
-    //         <li>
-    //           <button className="key big">delete</button>
-    //         </li>
-    //       </ol>
-    //     </div>
-    //     <div className="actions">
-    //       <span className="number">667359961</span>
-    //       {/* <!-- El botón de llamar debe tener la clase "activo" cuando --> */}
-    //       {/* <!-- el número de teléfono tiene 9 cifras --> */}
-    //       <a href="#" className="call">
-    //         Call
-    //       </a>
-    //       {/* <!-- Sólo se tiene que ver un botón u otro --> */}
-    //       <a href="#" className="hang active">
-    //         Hang
-    //       </a>
-    //     </div>
-    //   </main>
-    // </div>
   );
 }
 
