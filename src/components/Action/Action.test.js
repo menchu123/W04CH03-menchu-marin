6fa4d582777352650c95f6755acb4a75997d8a18
Menchu MV <mcmvillalobos@gmail.com>
2021-10-20 23:34:49 +0200

    Second Action test done

--- src/components/Action/Action.test.js
+++ src/components/Action/Action.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Action from "./Action";
 
 describe("Given an Action component", () => {
@@ -12,6 +13,19 @@ describe("Given an Action component", () => {
       const textElement = screen.getByText(expectedText);
 
       expect(textElement).toBeInTheDocument();
+    });
+  });
+
+  describe("When it recieves a function", () => {
+    test("Then it should execute the function if it is clicked", () => {
+      const text = "Juan";
+      const actionOnClick = jest.fn();
+
+      render(<Action text={text} actionOnClick={actionOnClick} />);
+      const textElement = screen.getByText(text);
+      userEvent.click(textElement);
+
+      expect(actionOnClick).toHaveBeenCalled();
     });
   });
 });
