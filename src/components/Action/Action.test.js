651a0df6064250333ad76937de8640cfa2e465c0
Menchu MV <mcmvillalobos@gmail.com>
2021-10-21 18:13:32 +0200

    Added context to Action test

--- src/components/Action/Action.test.js
+++ src/components/Action/Action.test.js
@@ -1,15 +1,23 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import Context from "../Context/Context";
 import Action from "./Action";
 
 describe("Given an Action component", () => {
   describe("When it recieves a 'test' text'", () => {
     test("Then it should display the text", () => {
+      // const { call, hang } = useContext(Context);
       const text = "text";
+      const className = "hang";
+      const isDisabled = true;
+      const call = jest.fn();
 
       const expectedText = "text";
 
-      render(<Action text={text} />);
+      render(
+        <Context.Provider value={{ call }}>
+          <Action text={text} className={className} isDisabled={isDisabled} />
+        </Context.Provider>
+      );
       const textElement = screen.getByText(expectedText);
 
       expect(textElement).toBeInTheDocument();
